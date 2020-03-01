3ea910282c5dba23599a5434f7bdf704f8ee2ec2
JordanAceto <jordanaceto@gmail.com>
2020-03-01 09:41:43 -0500

    initial commit

---
+++ template.h
@@ -0,0 +1,107 @@
+/*
+--|----------------------------------------------------------------------------|
+--| FILE DESCRIPTION:
+--|   template.h provides a template for well formatted c-header files.
+--|  
+--|----------------------------------------------------------------------------|
+--| NOTES:
+--|   None.
+--|  
+--|----------------------------------------------------------------------------|
+--| REFERENCES:
+--|   None.
+--|
+--|----------------------------------------------------------------------------|
+*/
+
+#ifndef TEMPLATE_H_INCLUDED
+#define TEMPLATE_H_INCLUDED
+
+/*
+--|----------------------------------------------------------------------------|
+--| INCLUDE FILES
+--|----------------------------------------------------------------------------|
+*/
+
+#include <stdint.h>
+
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC DEFINES
+--|----------------------------------------------------------------------------|
+*/
+
+/*
+--| NAME: SOME_DEFINITION
+--| DESCRIPTION: an arbitrary definitionas a demo
+--| TYPE: integer
+*/
+#define SOME_DEFINITION 42
+
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC TYPES
+--|----------------------------------------------------------------------------|
+*/
+
+/*
+--| NAME: Demo_Struct_t
+--| DESCRIPTION: an arbitrary struct as a demo
+*/
+typedef struct Demo_Struct_Type
+{
+    int some_member_ui32;
+    char some_other_member_ch;
+} Demo_Struct_t;
+
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC CONSTANTS
+--|----------------------------------------------------------------------------|
+*/
+
+/*
+--| NAME: SOME_CONSTANT_ui16
+--| DESCRIPTION: an arbitrary constant a demo
+--| TYPE: uint16_t
+*/
+const uint16_t SOME_CONSTANT_ui16 = 42;
+
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC VARIABLES
+--|----------------------------------------------------------------------------|
+*/
+
+/*
+--| NAME: some_variable_ui16
+--| DESCRIPTION: an arbitrary variable a demo
+--| TYPE: uint16_t
+*/
+uint16_t some_variable_ui16 = 42;
+
+/*
+--|----------------------------------------------------------------------------|
+--| PUBLIC FUNCTION PROTOTYPES
+--|----------------------------------------------------------------------------|
+*/
+
+/*------------------------------------------------------------------------------
+Function Name:
+    some_public_function
+
+Function Description:
+    Demonstrate the preferred function header template. 
+
+Parameters:
+    some_parameter: the arbitrary paramter.
+
+Returns:
+    uint8_t: the 8 bit unsigned return value.
+
+Assumptions/Limitations:
+    None
+------------------------------------------------------------------------------*/
+uint8_t some_public_function(uint8_t some_parameter);
+
+#endif
